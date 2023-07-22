15ba92cc880470d3419cc9117127a2397f09a0aa
Florian Zand <florianzand@me.com>
2023-07-23 01:18:27 +0200

    Update NSCollectionViewItem+.swift

--- Sources/AdvancedCollectionTableView/Configuration/Extensions/NSCollectionView+/NSCollectionViewItem+.swift
+++ Sources/AdvancedCollectionTableView/Configuration/Extensions/NSCollectionView+/NSCollectionViewItem+.swift
@@ -565,7 +565,7 @@ public extension NSCollectionViewItem {
 
 extension NSCollectionViewItem {
     open override func loadView() {
-        Swift.print("NSCollectionViewItem.loadView", self.nibName ?? "", self.nibBundle ?? "")
+        Swift.print("NSCollectionViewItem.loadView", self.nibName ?? "nil", self.nibBundle ?? "nil")
             self.view = NSView()
             self.observeCollectionItem()
     }
