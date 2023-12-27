5651033a07f13d4fb6ac8ffd852e7f489a00a52b
Florian Zand <florianzand@me.com>
2023-12-27 10:01:44 +0100

    updated

--- Sources/AdvancedCollectionTableView/Configuration/Configurations/NSListContentConfiguration/SecionHeaderView.swift
+++ Sources/AdvancedCollectionTableView/Configuration/Configurations/NSListContentConfiguration/SecionHeaderView.swift
@@ -153,13 +153,13 @@ open class SectionHeaderCell: NSView {
     func observeTableCellView() {
         guard tableCellObserver == nil else { return }
         tableCellObserver = self.observeChanges(for: \.superview, handler: {old, new in
-            Swift.print("SectionHeaderCell", self.superview ?? "nil")
+            Swift.print("SectionHeaderCell", self.superview ?? "nil", self.tableView ?? "nil")
             if self.contentConfiguration is NSListContentConfiguration {
                 self.rowView?.needsAutomaticRowHeights = true
                 self.tableView?.usesAutomaticRowHeights = true
             }
 
-            if let contentConfiguration = self.contentConfiguration as? NSListContentConfiguration, contentConfiguration.type == .automatic, let tableView = self.tableView, tableView.style == .automatic, contentConfiguration.tableViewStyle != tableView.effectiveStyle  {
+            if let contentConfiguration = self.contentConfiguration as? NSListContentConfiguration, contentConfiguration.type == .automaticRow, let tableView = self.tableView, tableView.style == .automatic, contentConfiguration.tableViewStyle != tableView.effectiveStyle  {
                 self.setNeedsUpdateConfiguration()
             }
             
