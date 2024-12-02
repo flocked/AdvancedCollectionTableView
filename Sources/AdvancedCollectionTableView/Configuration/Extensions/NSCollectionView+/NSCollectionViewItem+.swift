f6777e2c53db31c3614c86150b495e92a92ac8f6
Florian Zand <florianzand@me.com>
2024-12-02 05:30:16 +0100

    Update NSCollectionViewItem+.swift

--- Sources/AdvancedCollectionTableView/Configuration/Extensions/NSCollectionView+/NSCollectionViewItem+.swift
+++ Sources/AdvancedCollectionTableView/Configuration/Extensions/NSCollectionView+/NSCollectionViewItem+.swift
@@ -9,15 +9,6 @@ import AppKit
 import FZSwiftUtils
 import FZUIKit
 
-/*
-extension NSCollectionViewItem {
-    /// Instantiates a view for the item.
-    override open func loadView() {
-        view = NSView()
-    }
-}
-*/
-
 extension NSCollectionViewItem {
     // MARK: Configuring the background
 
